476ef19d5ed2dece55f03ac0f6a0fdbbb197b4d7
imyiit <imyiit@outlook.com>
2024-03-16 19:02:21 +0300

    Update folder structs

---
+++ src/database/setting.ts
@@ -0,0 +1,19 @@
+import type { SqLiteType, DatabaseSetting } from "./database";
+
+export class Setting<Value extends Record<string, SqLiteType>> {
+  table: string;
+  types: Value;
+  folder_name: string;
+  replace: boolean;
+  constructor({
+    table = "local",
+    types,
+    folder_name = "sqlite",
+    replace = false,
+  }: DatabaseSetting<Value>) {
+    this.table = table;
+    this.types = types;
+    this.folder_name = folder_name;
+    this.replace = replace;
+  }
+}
