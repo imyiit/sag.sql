22dfb3440a743be43103f996ff37d82370ab7052
1halon <1halon@users.noreply.github.com>
2024-04-30 02:52:20 +0300

    chore: remove dist directory

--- dist/joins/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./joins";
-export * from "./filter";
--- dist/settings/settings.d.ts
+++
@@ -1,8 +0,0 @@
-import type { SqLiteType, DatabaseSetting } from "../../types";
-export declare class Settings<Value extends Record<string, SqLiteType>> {
-    table: string;
-    types: Value;
-    folder_name: string;
-    replace: boolean;
-    constructor({ table, types, folder_name, replace, }: DatabaseSetting<Value>);
-}
